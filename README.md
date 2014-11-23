45e35a1d48ac04f16a807bdcddc8671a223d1831
j0lly <jos.ferraris@gmail.com>
2014-11-23 21:56:33 +0100

    added argparse ed lot of changes in input + output format

--- README.md
+++ README.md
@@ -5,18 +5,21 @@ This is a simple crawler that aim to surf the net and find open dns resolvers
 
 ## usage
 
-    python openresolver.py [Target] [Options] [Output]
+    python openresolver.py [-h] -t TARGET [-c CHUNK] -o OUTPUT [-v]
 
-    Target:
-        -t, --target target       Target IP or Range (CIDR format)
-    Options:
-        -h, --help                Show basic help message
-        -v, --version             Show program's version number
-        -c, --chunk               Numer of IP Range's chunk to use
-    Output: 
-        -o, --output file         Print log on a file
+    OpenResolver: an Open DNS Resolver crawler.
 
-    Examples:
-        python openresolver.py -t 173.194.40.1
+    optional arguments:
+        -h, --help            show this help message and exit
+        -t TARGET, --target TARGET
+                        Set target: IP or CIDR range.
+        -c CHUNK, --chunk CHUNK
+                        Set Ip Range chunk size.
+        -o OUTPUT, --output OUTPUT
+                        Output file
+        -v, --version         show program's version number and exit
+
+##  Examples
+        python openresolver.py -t 173.194.40.1 -o out.txt
         python openresolver.py -t 173.194.40.1/25 -c 50 -o results.txt 
         python openresolver.py -v
--- openresolver.py
+++ openresolver.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# filename: openresolver.py
+#
+# author : j0lly [at] anche.no
+# Open DNS Resolver crwaler
+# For fun and profit
+#
+# Lcense BSD
 
 from Queue import Queue
 import dns.resolver
 import threading
 import iptools
 import sys
+import argparse
 
-
+__version__ = '0.3'
+__author__ = 'J0lly'
+__date__ = '2014-11-23'
+__email__ = 'j0lly@anche.no'
 
 class thread_resolve (threading.Thread):
 
@@ -49,33 +62,45 @@ def main():
                         iptools.ipv4.PRIVATE_NETWORK_192_168, iptools.ipv4.RESERVED
                         ]
 
+        parser = argparse.ArgumentParser(description='OpenResolver: an Open DNS Resolver crawler.')
+        parser.add_argument("-t", "--target", dest="target", action="store", type=str,
+                default=None, help="Set target: IP or CIDR range.", required=True)
+        parser.add_argument("-c", "--chunk", dest="chunk", action="store",
+                        default="100", help="Set Ip Range chunk size.")
+        parser.add_argument("-o", "--output", dest="output", action="store",
+                        help="Output file", required=True)
+        parser.add_argument('-v', '--version', action='version',
+                                    version='%(prog)s ' + __version__)
+
+        args = parser.parse_args()
+
         ### sanity checks ###
-        if not iptools.ipv4.validate_ip(sys.argv[1]) and not iptools.ipv4.validate_cidr(sys.argv[1]):
+        if not iptools.ipv4.validate_ip(args.target) and not iptools.ipv4.validate_cidr(args.target):
                 print 'not a valid ip or network -.- '
                 sys.exit()
-        if iptools.ipv4.validate_ip(sys.argv[1]) :
+        if iptools.ipv4.validate_ip(args.target) :
             for net in bad_networks:
-                if sys.argv[1] in iptools.IpRange(net) :
+                if args.target in iptools.IpRange(net) :
                     print 'reserved ip, dumb!'
                     sys.exit()
-        elif iptools.ipv4.validate_cidr(sys.argv[1]):
+        elif iptools.ipv4.validate_cidr(args.target):
             for net in bad_networks:
-                if iptools.IpRange(sys.argv[1]).__hash__() == iptools.IpRange(net).__hash__() :
+                if iptools.IpRange(args.target).__hash__() == iptools.IpRange(net).__hash__() :
                     print 'reserved network, dumb!'
                     sys.exit()
 
         ### real script ###
-        if iptools.ipv4.validate_ip(sys.argv[1]) :
-            resolve(sys.argv[1])
+        if iptools.ipv4.validate_ip(args.target) :
+            resolve(args.target)
 
         else :
 
             # Get ip range mask
-	    iprange = iptools.IpRange(sys.argv[1])
+	    iprange = iptools.IpRange(args.target)
 	    queue = Queue()
 	    resolvers = []
 
-	    for ip_chunks in chunker(iprange, int(sys.argv[2])) :
+	    for ip_chunks in chunker(iprange, int(args.chunk)) :
                 ### new dictionary and Queue ###
                 threads = {}
                 for ip in ip_chunks :
@@ -85,14 +110,16 @@ def main():
 
 	        for thread in threads:
 		    threads[thread].join()
-		
+
 		while queue.qsize() > 0:
 
 		    resolvers.append(queue.get())
 	    if resolvers :
-	    	print 'I found %s resolver!'%(len(resolvers))	
-	    	print resolvers
-		print 'dumped list on file out.txt'
+	    	print 'I found %s resolvers!'%(len(resolvers))
+                with open( args.output, 'w') as out_file:
+                    out_file.write("\n".join(map(lambda x: str(x), resolvers)))
+                    out_file.write("\n")
+		print 'dumped list to %s '%(args.output)
 
 if __name__ == "__main__":
     main()
