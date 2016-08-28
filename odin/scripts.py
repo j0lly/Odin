d55441dcf7ae0aff985666f267c92ea5d7da0e39
j0lly <jos.ferraris@gmail.com>
2016-08-28 01:34:46 +0100

    * added dump, load, delete to odin Cli tool * make scan more lean

--- odin/scripts.py
+++ odin/scripts.py
@@ -34,6 +34,8 @@ def get_args():
     parser.add_argument("-V", "--version", action="version",
                         version="%(prog)s " + __version__)
     subparsers = parser.add_subparsers(dest='subparser')
+
+    # Scan sub parser
     scan = subparsers.add_parser('scan',
                                  description="{}{}{}".format(
                                      "Perform a scan and return the ",
@@ -45,15 +47,17 @@ def get_args():
     scan.add_argument("-c", "--chunk", dest="chunk", action="store",
                       default=50, type=int,
                       help="Set Ip Range chunk size; 1024 max.")
-    scan.add_argument("-o", "--output", dest="output", action="store",
-                      help="Output file", required=False)
     scan.add_argument("-f", "--filter", dest="filter", action="store",
                       help='{} {} {}'.format(
                           "show only if target is (or has):",
                           "is_dns, is_resolver, version, all.",
                           "It defaults to 'is_dns'."),
-                      required=False, default='is_dns', type=str)
+                      required=False, default='is_resolver', type=str)
+    scan.add_argument("--store", action="store_true",
+                      help="if specified store the result in Dynamo")
     scan.add_argument("-v", "--verbose", action="count")
+
+    # Query sub parser
     query = subparsers.add_parser('query',)
     query.add_argument("-t", "--target", dest="target",
                        action="store", type=str, default=None,
@@ -61,17 +65,31 @@ def get_args():
     query.add_argument("-c", "--chunk", dest="chunk", action="store",
                        default=50, type=int,
                        help="Set Ip Range chunk size; 1024 max.")
-    query.add_argument("-o", "--output", dest="output", action="store",
-                       help="Output file", required=False)
     query.add_argument("-f", "--filter", dest="filter", action="store",
                        help='{} {} {}'.format(
                            "show only if target is (or has):",
                            "is_dns, is_resolver, version, all.",
                            "It defaults to 'is_resolver'."),
                        required=False, default='is_resolver', type=str)
-    query.add_argument("-v", "--verbose", action="count")
 
-    return parser.parse_args()
+    # Dump sub parser
+    dump = subparsers.add_parser('dump',)
+    dump.add_argument("-o", "--output", dest="output", action="store",
+                      help="Output file", required=True)
+
+    # Load sub parser
+    load = subparsers.add_parser('load',)
+    load.add_argument("-i", "--input", dest="output", action="store",
+                      help="Input file", required=True)
+
+    # Delete sub parser
+    delete = subparsers.add_parser('delete',)
+    delete.add_argument("-t", "--target", dest="target",
+                        action="store", type=str, default=None,
+                        help="Set target for deletion: IP or CIDR range.",
+                        required=True)
+
+    return (parser.parse_args(), parser)
 
 
 def test_args(args):
@@ -94,9 +112,6 @@ def test_args(args):
     assert args.chunk > 1 and args.chunk <= 1024, (
         'You have to specify a chunk between 1 and 1024.')
     result = utils.chunker(targets, args.chunk)
-    assert args.filter in ['is_dns', 'is_resolver', 'version', 'all'], (
-        'You have to specify a filter in: is_dns, is_resolver, version, all.')
-
     return result
 
 
@@ -123,40 +138,84 @@ def load_data():
 def main():
     """ the main script."""
 
-    args = get_args()
+    args, parser = get_args()
 
+    # SCAN case
     if args.subparser == "scan":
         targets = test_args(args)
+        assert args.filter in ['is_dns',
+                               'is_resolver',
+                               'version',
+                               'all'], ('{}{}'.format(
+                                   'You have to specify a filter in:',
+                                   ' is_dns, is_resolver, version, all.'))
+
         my_queue = queue.Queue()
         result = []
         printing = {}
+
         for obj in run_scan(args.filter, my_queue, targets):
             printing[obj.ip] = obj.serialize
             result.append(obj)
-        if args.output:
-            if os.path.exists(args.output):
-                answer = input(
-                    'file {} exist: overwrite? '.format(args.output))
-                if answer in ['yes', 'y']:
-                    with open(args.output, 'w') as output:
-                        # FIXME need to deserialize
-                        output.write(str(result))
-                else:
-                    print('\nnot overwriting the file; print to stout only\n')
-        try:
-            with OpenDnsModel.batch_write() as batch:
-                for ip in result:
-                    batch.save(ip)
-        except:
-            print('batch failed to save to db')
+
+        if args.store:
+            try:
+                with OpenDnsModel.batch_write() as batch:
+                    for ip in result:
+                        batch.save(ip)
+            except:
+                print('batch failed to save to db')
 
         pprint(printing)
 
+    # QUERY case
     elif args.subparser == "query":
         pass
+
+    # LOAD case
+    elif args.subparser == "load":
+        try:
+            OpenDnsModel.load(args.input)
+        except FileNotFoundError:
+            print('{}{}{}'.format(
+                '\nUnable to load data from',
+                args.input,
+                '. are you sure the file exisits?\n')
+                 )
+
+    # DUMP case
+    elif args.subparser == "dump":
+        if os.path.exists(args.output):
+            answer = input(
+                'file {} exist: overwrite? '.format(args.output))
+            if answer in ['yes', 'y']:
+                OpenDnsModel.dump(args.output)
+            else:
+                print('\nnot overwriting the file; print to stout only\n')
+        else:
+            OpenDnsModel.dump(args.output)
+
+    # DELETE CASE
+    elif args.subparser == "delete":
+        targets = utils.findip(args.target)
+        # FIXME BUG in pynamo: single delete is ok, but batching complain about
+        # key that cannnot be null
+        objects = []
+        for target in targets:
+            try:
+                objects.append(OpenDnsModel.get(target))
+            # obj does not exist
+            except:
+                pass
+        try:
+            with OpenDnsModel.batch_write() as batch:
+                for ip in objects:
+                    batch.delete(ip)
+        except Exception as err:
+            print("unable to delete the specified ips: {}".format(err))
+
     else:
-        # FIXME print help
-        pass
+        parser.print_help()
 
 
 if __name__ == "__main__":
